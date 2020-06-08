3d5c9a77e747687b04de8f5d258e768dc50b40a0
sangho15 <sangho15@naver.com>
2020-06-08 15:16:42 +0900

    for sunkist

--- src/Server.java
+++ src/Server.java
@@ -1,21 +1,29 @@
+import java.util.ArrayList;
+
 import kr.ac.konkuk.ccslab.cm.sns.CMSNSUserAccessSimulator;
 import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
 
 public class Server {
 	
-	CMServerStub m_serverStub;
-	CMServerEventHandler m_eventHandler;
-	CMClientEventHandler m_eventHandler2;
-	boolean m_bRun;
+	private CMServerStub m_serverStub;
+	private CMServerEventHandler m_eventHandler;
+	private boolean m_bRun;
+	private int playerCount;
+	private GameManager[] GM;
 	
 	public Server()
 	{
 		m_serverStub = new CMServerStub();
 		m_eventHandler = new CMServerEventHandler(m_serverStub);
-		m_eventHandler2 = new CMClientEventHandler();
 		m_bRun = true;
+		playerCount = 0;
 	}
-
+	public void CreateGM() {
+		
+	}
+	public void DeleteGM() {
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Server server = new Server();
